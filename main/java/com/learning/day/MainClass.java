95cf725fab0d3458222dc46e6a9a1aba05bdba62
Sienkiewicz <SG0221962@sabre.com>
2016-03-11 09:40:10 +0100

    Initial commit

---
+++ main/java/com/learning/day/MainClass.java
@@ -0,0 +1,7 @@
+package com.learning.day;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("working");
+    }
+}
---
+++ test/java/com/learning/day/MainClassTest.java
@@ -0,0 +1,11 @@
+package com.learning.day;
+
+import org.junit.Test;
+
+public class MainClassTest {
+
+    @Test
+    public void createClassPasses() throws Exception {
+        MainClass mainObject = new MainClass();
+    }
+}
